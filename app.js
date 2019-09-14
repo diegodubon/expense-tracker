75a15e8228f4bb1ebe78040c10760df5860c758d
Diego <diego.dubon@laureate.net>
2019-09-14 11:17:33 -0600

    added posgresql connection

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-if (process.env.NODE_ENV === "developement") {
-  require("dotenv/config");
-}
-
+require("dotenv/config");
+const Sequelize = require("sequelize");
 const express = require("express");
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
-});
+const appInit = async () => {
+  try {
+    const sequelize = new Sequelize(
+      "postgres://hylagfku:WOd63CDTMrEGaEfSZxyIKW-b_athTQsR@salt.db.elephantsql.com:5432/hylagfku"
+    );
+
+    await sequelize.authenticate();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Running on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("error", error);
+  }
+};
+
+appInit();
