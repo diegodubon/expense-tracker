5f75546aeae92e327a1638d72411fc8f21cba984
Diego <diego.dubon@laureate.net>
2019-09-14 11:05:07 -0600

    working on eslint

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-if (process.env.NODE_ENV === 'developement') {
-  require('dotenv/config');
+if (process.env.NODE_ENV === "developement") {
+  require("dotenv/config");
 }
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}
-`);
+  console.log(`Server Running on ${PORT}`);
 });
