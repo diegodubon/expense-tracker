f2e09bf2b09d54bd94394fd731b492e26dca4bc0
Diego <diego.dubon@laureate.net>
2019-09-14 19:03:16 -0600

    added supervisor for hot reload and config file

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-require("dotenv/config");
+let confi = {};
+
+let NODE_ENV = process.env.NODE_ENV || "development";
+if (NODE_ENV === "development") {
+  require("dotenv/config");
+  config = require("./config");
+  config = config["dev"];
+}
+
 const Sequelize = require("sequelize");
 const express = require("express");
 
 const app = express();
-const config = require("./config");
+
 const appInit = async () => {
   try {
-    const sequelize = new Sequelize(config.dev.db.uri);
+    const { DB_URI, PORT } = config;
+    console.log(config);
+    const sequelize = new Sequelize(DB_URI, {
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    });
 
-    await sequelize.authenticate();
-    app.listen(process.env.PORT, () => {
-      console.log(`Server Running on ${process.env.PORT}`);
+    await sequelize.authenticate().catch(error => {
+      console.error("Unable to connect to the database:", err);
+    });
+    console.log("Connection has been established successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server Running on ${PORT}`);
     });
   } catch (error) {
     console.error("error", error);
--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 module.exports = {
   dev: {
-    PORT: 5000,
+    PORT: process.env.PORT || 5000,
     NODE_ENV: "development",
-    db: {
-      uri:
-        "postgres://hylagfku:WOd63CDTMrEGaEfSZxyIKW-b_athTQsR@salt.db.elephantsql.com:5432/hylagfku"
-    }
+    DB_URI:
+      process.env.DB_URI ||
+      "postgres://hylagfku:WOd63CDTMrEGaEfSZxyIKW-b_athTQsR@salt.db.elephantsql.com:5432/hylagfku"
   }
 };
