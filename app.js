6f7ba1200cee004067b5daeeb52b25afab9a3c4c
Diego <diego.dubon@laureate.net>
2019-09-22 12:46:33 -0600

    added backend for front production build and heroku script build

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ const appInit = async () => {
     });
     console.log("Connection has been established successfully.");
 
+    if (NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname, "client/build")));
+      app.get("*", (req, res) => {
+        res.sendfile(path.join((__dirname = "client/build/index.html")));
+      });
+    }
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname + "/client/public/index.html"));
     });
